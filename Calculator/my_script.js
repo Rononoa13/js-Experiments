ebd87476c25685a19d7f663ae93d6407a445a617
Rononoa13 <sumitluitel42@gmail.com>
2020-01-29 20:08:23 +0545

    DOM manipulation to submit number on form.

---
+++ Calculator/my_script.js
@@ -0,0 +1,13 @@
+function insert(num)
+{
+  document.form.textview.value = document.form.textview.value + num;
+
+  
+    //document.getElementById('demo').value;
+
+    //alert("Hello! I am an alert box!");
+
+    //return num;
+    //document.form.textview
+}
+//document.getElementById('demo').onclick = insert;
