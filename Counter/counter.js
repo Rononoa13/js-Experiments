d94a8e57cfda0a94f41b8f7314556c1a7c793ce1
Rononoa13 <sumitluitel42@gmail.com>
2020-06-10 00:41:15 +0545

    JS file that increments, decrements, and reset the counter.

---
+++ Counter/counter.js
@@ -0,0 +1,36 @@
+const count = document.getElementById("count");
+// returns Nodelist(3)
+const btns = document.querySelectorAll(".btn-");
+
+// set initial count
+let counter = 0
+
+count.innerHTML = counter;
+
+btns.forEach(btn => {
+    console.log(btn);
+    btn.addEventListener("click", function(e) {
+        // console.log(e.currentTarget.classList); // Identifies current target for the event. 
+        const classOfBtn = e.currentTarget.className; 
+        // console.log(typeof(className)); // is of type string
+        console.log("Variable className -> " + classOfBtn);
+        if (classOfBtn === "btn- increase") {
+            counter++;
+        }
+        else if (classOfBtn === "btn- decrease") {
+            counter--;
+        }
+        else {
+            counter = 0;
+        }
+        count.textContent = counter;
+
+        // This is another logix
+        // if (classOfBtn.contains("increase")) {
+        //     counter++;
+        // }
+        // count.textContent = counter;
+
+    })
+});
+
