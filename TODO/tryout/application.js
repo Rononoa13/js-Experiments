08a6d9ea7e54d44a4a72a224ad40a84b5f774a5e
Rononoa13 <sumitluitel42@gmail.com>
2020-06-18 23:29:10 +0545

    Disable button if empty input field else enabled

--- TODO/tryout/application.js
+++ TODO/tryout/application.js
@@ -1,8 +1,19 @@
 const task = document.getElementById("task");
-const btn = document.getElementById("btn")
+const btn = document.getElementById("btn");
+
 // when you want value of input field ".value"
+//input event that fires every time value changes.
 
+task.addEventListener("input", function () {
+  if (task.value.length == 0) {
+    btn.disabled = true;
+  } else {
+    btn.disabled = false;
+  }
+});
 
-btn.addEventListener("click", function() {
-    console.log(task.value);
-})
+btn.addEventListener("click", function () {
+  console.log(task.value);
+  btn.disabled = true;as
+  task.value = " ";
+});
